4ce3e04ece64dbcac3fb1d1b7317a679e752c420
AbuZaid55 <zaid70979@gmail.com>
2026-06-25 22:56:42 +0530

    DataTypes

---
+++ 2_primitiveDataTypes.java
@@ -0,0 +1,31 @@
+class DataTypes {
+    public static void main(){
+        // Interger data type
+        byte age = 127;   // -128 to 127
+        short year = 2026;
+        int marks = 90;
+        long population = 8000000000L;
+
+        // deciaml value
+        double salary = 50000.10; // by default decimal value is in double
+        float pi = 3.14f;
+
+        // characters
+        char grade = 'A';
+
+        // boolean
+        boolean isPassed = true;
+
+        // Print 
+        System.out.println("Age " + age);
+        System.out.println("Year " + year);
+        System.out.println("Marks " + marks);
+        System.out.println("Population " + population);
+
+        System.out.println("Salary " + salary);
+        System.out.println("Pi " + pi);
+
+        System.out.println("Grade " + grade);
+        System.out.println("isPassed " + isPassed);
+    }
+}
