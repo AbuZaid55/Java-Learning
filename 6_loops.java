70e3353aee82c99296a61cbac843b8a2787bf197
AbuZaid55 <zaid70979@gmail.com>
2026-06-26 18:38:33 +0530

    loops

---
+++ 6_loops.java
@@ -0,0 +1,31 @@
+class Loops {
+    public static void main(String[] args) {
+        
+        // for loops
+        // for (int i= 0; i<=10; i++){
+        //     System.out.println(i);
+        // }
+
+        // int i = 0;
+        // for (; i<=10;){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // While loop
+        // int i = 0;
+        // while(i<=10){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // Do while loop
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+
+
+    }
+}
