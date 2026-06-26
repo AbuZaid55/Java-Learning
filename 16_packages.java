c27405f980efccbfec0aba34642f8bf93cfc8603
AbuZaid55 <zaid70979@gmail.com>
2026-06-27 01:59:21 +0530

    packages

---
+++ 16_packages.java
@@ -0,0 +1,10 @@
+// import tool.AdvanceCalc;
+import tool.*;
+
+class Packages {
+    public static void main(String[] args) {
+        AdvanceCalc advanceCalc = new AdvanceCalc();
+        System.out.println(advanceCalc.divide(5, 2));
+        System.out.println(advanceCalc.add(5, 2));
+    }
+}
---
+++ tool/AdvanceCalc.java
@@ -0,0 +1,7 @@
+package tool;
+
+public class AdvanceCalc extends Calc {
+    public double divide (double a, double b){
+        return a/b;
+    }
+}
