56a690f765e8158a9796f1c64dc7565086ca71a7
AbuZaid55 <zaid70979@gmail.com>
2026-06-27 15:13:38 +0530

    polymorphism

---
+++ 18_polymorphism.java
@@ -0,0 +1,9 @@
+class Polymorphism {
+    public static void main(){
+        // Polymorphism -
+        // Same object or method behave differently
+        // Tow type of polymorphism -
+        // 1. Compile time polymorphism -> Method overloading
+        // 2. Run time polymorphism -> Method overriding
+    }
+}
